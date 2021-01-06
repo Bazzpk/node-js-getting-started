349737095db70e2aa4f60c642426d503142ab99a
Worsts <worsts2002@gmail.com>
2021-01-06 20:17:37 +0700

    OK

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require('express')
+const line = require('@line/bot-sdk');
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+const config = {
+  channelAccessToken: 'hM9udHFKbfItO690aGJsC+K2+e1S6fYPAKtpjS69WLcXVMYaaprzNVhuDnu0xKxrcI1Sp6WmtXc67GcK/I2FB1aVxKgRVoOauSczVuhvStrgQoZthquemXEo9BCtJQyj44hIQhZtPXGP7cwuCpvOPQdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '497bf56799d2d1558380940e07a03c7d'
+};
+
+const app = express();
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text
+  });
+}
+
+app.listen(8080);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
